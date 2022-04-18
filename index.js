020e68b89d2ebd8d66dea0925629992d0112e1d7
Omer Lev-Ron <omeround3@gmail.com>
2022-04-18 18:20:00 +0000

    Created new class for improtCVEDetails and added functionlity to automaticalliy detect current year and import all CVE data from NVD until this year

--- index.js
+++ index.js
@@ -1,59 +1,88 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const https = require("https");
-const JSONStream = require('JSONStream');
-const zlib = require("zlib")
 // const through2Batch = require('through2-batch');
-const es = require('event-stream');
-const streamToMongoDB = require('stream-to-mongo-db').streamToMongoDB;
-const CVEMeta = require('./app/models/cvemeta.model.js');
-const schedule = require('node-schedule');
+const CVEMeta = require("./app/models/cvemeta.model.js");
+const schedule = require("node-schedule");
+const importCVEDetails =
+  require("./app/services/cve-import.js").importCVEDetails;
 
 app.use(bodyParser.json());
 
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 // Import data from NIST for years provided in cmd line args
-const destDBConfig = { dbURL: dbConfig.url, collection: 'cvedetails', batchSize: 500};
+const destDBConfig = {
+  dbURL: dbConfig.url,
+  collection: "cvedetails",
+  batchSize: 500,
+};
 var years = process.argv.slice(2);
-//Import files from NIST
+// Import files from NIST
 if (years.length > 0) {
-  console.log("Importing JSON feeds from NVD for the years: " + years)
-  for(var i=0;i<years.length;i++) {
-    var url = 'https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-' + years[i] + '.json.gz';
-    console.log("Importing " + years[i])
+  console.log("Importing JSON feeds from NVD for the years: " + years);
+  for (var i = 0; i < years.length; i++) {
+    var url =
+      "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-" +
+      years[i] +
+      ".json.gz";
+    console.log("Importing " + years[i]);
+    importCVEDetails(url, destDBConfig);
+  }
+} else {
+  var FIRST_YEAR = 2002;
+  let currentYear = new Date().getFullYear();
+  const yearsRange = [...Array(currentYear - FIRST_YEAR + 1).keys()].map(
+    (x) => x + FIRST_YEAR
+  );
+  console.log(
+    `Importing JSON feeds from NVD for the years ${yearsRange[0]} - ${
+      yearsRange[yearsRange.length - 1]
+    }`
+  );
+
+  for (const year in yearsRange) {
+    console.log(`Importing year ${yearsRange[year]}`);
+    var url =
+      "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-" +
+      yearsRange[year] +
+      ".json.gz";
     importCVEDetails(url, destDBConfig);
   }
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log("Successfully connected to the database");
-}).catch(err => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
 
-require('./app/routes/cvedetails.routes.js')(app);
+require("./app/routes/cvedetails.routes.js")(app);
 
-const handleError = function(err) {
+const handleError = function (err) {
   console.error(err);
   // handle your error
 };
 
-var firstRec = new CVEMeta({ lastModifiedDate: new Date() });
-firstRec.save(function (err) {
-  if (err) return handleError(err);
-});
+// var firstRec = new CVEMeta({ lastModifiedDate: new Date() });
+// firstRec.save(function (err) {
+//   if (err) return handleError(err);
+// });
 
-CVEMeta.findOne({}, (err, record) => {
-  console.log(record);
-});
+// CVEMeta.findOne({}, (err, record) => {
+//   console.log(record);
+// });
 
 // Set the scheduler to synchronize with nist db 0 */2 * * *
 // var j = schedule.scheduleJob('* * * * *', () => {
@@ -71,32 +100,7 @@ CVEMeta.findOne({}, (err, record) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() =>
+app.listen(PORT, () =>
   //http://localhost:3000/cvedetails/CVE-2017-0001
   console.log(`Server is running and listening on port ${PORT}.`)
 );
-
-
-function importCVEDetails(url, destDBConfig) {
-  // const destDBConfig = { dbURL :dbConfig.url, collection : 'cvedetails', batchSize: 500};
-  // const url = 'https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz';
-
-  https.get(url, response => response
-          .pipe(zlib.createGunzip())
-          .pipe(JSONStream.parse('CVE_Items.*'))
-          .pipe(es.map(function (doc, callback) {
-              delete doc.configurations;
-              doc._id = doc.cve.CVE_data_meta.ID;
-              callback(null, doc)
-          }))
-          .pipe(streamToMongoDB(destDBConfig))
-          // .pipe(through2Batch.obj({ batchSize: 500 }))
-          // .pipe(es.mapSync(function(documents){
-          //     console.log(documents.length);
-          //     cvedetails.insertMany(documents);
-          // }))
-          .on('end', function () {
-              console.log('Done importing CVE JSON Feeds');
-          })
-  );
-}
