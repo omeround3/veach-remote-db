1405f5a1ed33f8a7634e222ca279c79bcc08306c
Omer Lev-Ron <omeround3@gmail.com>
2022-04-29 09:51:58 +0000

    Renamed classes

--- app/routes/cvedetails.js
+++ app/routes/cvedetails.js
@@ -1,4 +1,4 @@
 module.exports = (app) => {
-    const cvedetails = require('../controllers/cvedetails.controller.js');
+    const cvedetails = require('../controllers/cvedetails.js');
     app.get('/cvedetails/:cveid', cvedetails.findOne);
 }
